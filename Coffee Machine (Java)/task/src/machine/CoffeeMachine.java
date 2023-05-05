0feaca1c974274e0c373a2debd394b3683d84211
BogdanKaliuzhniy <kaliuzhnb@gmail.com>
2023-05-05 10:50:55 +0300

    Stage 1/6

---
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,17 @@
+package machine;
+import java.util.Scanner;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*System.out.println("""
+                Starting to make a coffee
+                Grinding coffee beans
+                Boiling water
+              / Mixing boiled water with crushed coffee beans
+                Pouring coffee into the cup
+                Pouring some milk into the cup
+                Coffee is ready!"""); */
+        System.out.println("Write how many cups of coffee you will need:");
+    }
+}
